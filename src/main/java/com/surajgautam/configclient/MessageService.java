6787b9c3863c27622bf03f146c58b9744a6b9e08
surajgautam <teamwarrior200@gmail.com>
2019-03-30 13:45:19 +0545

    Organize imports

--- src/main/java/com/surajgautam/configclient/MessageService.java
+++ src/main/java/com/surajgautam/configclient/MessageService.java
@@ -1,7 +1,6 @@
 package com.surajgautam.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 /**
