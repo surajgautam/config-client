f84c7ff433087892ecf52a95f1acd5a38de0359c
surajgautam <teamwarrior200@gmail.com>
2019-03-30 14:14:15 +0545

    Add refresh scope again

--- src/main/java/com/surajgautam/configclient/MessageService.java
+++ src/main/java/com/surajgautam/configclient/MessageService.java
@@ -1,12 +1,14 @@
 package com.surajgautam.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by Suraj Gautam.
  */
 @Component
+@RefreshScope
 public class MessageService {
 
     @Value("${message}")
